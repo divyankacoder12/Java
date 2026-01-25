96b27bb9fdee2977d371f6a5cf3d8152f4c3ec36
divyankacoder12 <divyankasagolale@gmail.com>
2026-01-25 21:28:43 +0530

    dataTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+    
+    byte age = 19;
+    short students = 2000;
+    int a = 1;
+    float pi = 3.14f;
+    long lightYears = 9460730472580800L;
+    char grade = 'A';
+    double digit = 6.2034;
+    String name = "abc";
+    boolean flag = true;
+    System.out.println(age);
+    System.out.println(students);
+    System.out.println(a);
+    System.out.println(pi);
+    System.out.println(lightYears);
+    System.out.println(grade); 
+    System.out.println(digit);   
+    System.out.println(name);
+    System.out.println(flag);    
+  }
+}
